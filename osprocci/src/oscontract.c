8039b81ee2021df84bcc184f2d7cd6b36b85e6da
root <root@uwin.insead.edu>
2011-12-24 00:10:06 +0100

    Normalisation of Units for Quantities T,G,M,K for Compute and Storage

--- osprocci/src/oscontract.c
+++ osprocci/src/oscontract.c
@@ -93,6 +93,48 @@ private	int	terminate_openstack_contract( int status, struct cords_oscontract * 
 	return( status );
 }
 
+
+/*	---------------------------------------------------	*/
+/*		o s _ n o r m a l i s e _ v a l u e 		*/
+/*	---------------------------------------------------	*/
+/*	this function takes a value string as its parameter	*/
+/*	which may or not be terminated by a T,G,M,K which	*/
+/*	indicates an explicite quantity. A default quantity	*/
+/*	type is passed as the second parameter to be used	*/
+/*	in case no explicite value is present.			*/
+/*	The function returns the normalisation of the value	*/
+/*	---------------------------------------------------	*/
+private	int	os_normalise_value( char * sptr, int normal )
+{
+	int	factor=1;
+	int	value=0;
+	if ( normal == 'T' )
+		factor = 1000000000;
+	else if ( normal == 'G' )
+		factor = 1000000;
+	else if ( normal == 'M' )
+		factor = 1000;
+	else if ( normal == 'K' )
+		factor = 1;
+	else	factor = 1;
+
+	value = atoi(sptr);
+
+	while (( *sptr >= '0' ) && ( *sptr <= '9' )) sptr++;
+
+	if ( *sptr == 'T' )
+		factor = 1000000000;
+	else if ( *sptr == 'G' )
+		factor = 1000000;
+	else if ( *sptr == 'M' )
+		factor = 1000;
+	else if ( *sptr == 'K' )
+		factor = 1;
+
+	return( value * factor );
+}
+
+
 /*	-----------------------------------------------------------------	*/
 /*		r e s o l v e _ c o n t r a c t _ f l a v o r 			*/
 /*	-----------------------------------------------------------------	*/
@@ -115,22 +157,22 @@ private	char *	resolve_contract_flavor( struct cords_oscontract * cptr )
 	if (!( vptr = cords_extract_atribut( cptr->compute.message, "occi", 
 		_CORDS_COMPUTE, _CORDS_MEMORY ) ))
 		request.memory = 0;
-	else	request.memory = atoi( vptr );
+	else	request.memory = os_normalise_value( vptr,'G' );
 
 	if (!( vptr = cords_extract_atribut( cptr->compute.message, "occi", 
 		_CORDS_COMPUTE, _CORDS_CORES ) ))
 		request.cores = 0;
-	else	request.cores = atoi( vptr );
+	else	request.cores = os_normalise_value( vptr,'U' );
 
 	if (!( vptr = cords_extract_atribut( cptr->compute.message, "occi", 
 		_CORDS_COMPUTE, _CORDS_SPEED ) ))
 		request.speed = 0;
-	else	request.speed = atoi(vptr);
+	else	request.speed = os_normalise_value(vptr,'G');
 	
 	if (!( vptr = cords_extract_atribut( cptr->storage.message, "occi", 
 		_CORDS_STORAGE, _CORDS_SIZE ) ))
 		request.storage = 0;
-	else	request.storage = atoi(vptr);
+	else	request.storage = os_normalise_value(vptr,'G');
 	
 	/* ----------------------------------------- */
 	/* for structures in flavor message response */
@@ -148,16 +190,16 @@ private	char *	resolve_contract_flavor( struct cords_oscontract * cptr )
 		else	flavor.id = vptr;
 		if (!( vptr = json_atribut( dptr, "disk" ) ))
 			flavor.storage = 0;
-		else	flavor.storage = atoi(vptr);
+		else	flavor.storage = os_normalise_value(vptr,'G');
 		if (!( vptr = json_atribut( dptr, "ram" ) ))
 			flavor.memory = 0;
-		else	flavor.memory = atoi(vptr);
+		else	flavor.memory = os_normalise_value(vptr,'M');
 		if (!( vptr = json_atribut( dptr, "vcpus" ) ))
 			flavor.cores = 0;
-		else	flavor.cores = atoi(vptr);
+		else	flavor.cores = os_normalise_value(vptr,'U');
 		if (!( vptr = json_atribut( dptr, "speed" ) ))
 			flavor.speed = 0;
-		else	flavor.speed = atoi(vptr);
+		else	flavor.speed = os_normalise_value(vptr,'G');
 		/* ------------------------------------ */
 		/* compare the request and the response */
 		/* ------------------------------------ */
