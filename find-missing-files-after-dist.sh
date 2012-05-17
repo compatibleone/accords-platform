4200d9063e370b50146e7b1b6462213b1441c7e4
Alexandre Lissy <alissy@mandriva.com>
2012-05-17 17:28:19 +0200

    Enhancing the missing files discovery script

--- find-missing-files-after-dist.sh
+++ find-missing-files-after-dist.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-BUILD_DIR=build
-TAR_DIR="accords-2.0"
-TAR_NAME="${TAR_DIR}.tar.gz"
+SOURCE_DIR=$(pwd)
+ORIG_DIR=$(mktemp -d)
+BUILD_DIR=$(mktemp -d)
 TMP_SOURCE=$(mktemp)
 TMP_DEST=$(mktemp)
 
-mkdir -p ${BUILD_DIR} || (echo "Cannot create build dir ${BUILD_DIR}" && exit 1)
-
-./configure && make dist || (echo "Cannot create tarball" && exit 2)
-
-pushd ${BUILD_DIR}/
-	tar xf ../${TAR_NAME} -C . || (echo "Cannot extract tarball" && exit 3)
-	cd ${TAR_DIR} && ./configure || (echo "Cannot configure target" && exit 4)
+pushd ${ORIG_DIR}
+	make -C ${SOURCE_DIR} distclean || echo "No distclean"
+	${SOURCE_DIR}/configure && make dist && make distclean || (echo "Cannot create tarball" && exit 2)
+	VERSION=$(grep PACKAGE_VERSION ${SOURCE_DIR}/configure | grep '=' | cut -d'=' -f2 | sed -e "s/'//g")
 popd
 
-SOURCE_FILES=$(find . -type f -prune -not -path "./${BUILD_DIR}/*" -and -prune -not -path "./.git/*" -and -prune -not -name ".gitignore" -and -prune -not -name "${TAR_NAME}" > ${TMP_SOURCE})
-DEST_FILES=$(find ./${BUILD_DIR}/ -type f | sed -e "s|${BUILD_DIR}\/${TAR_DIR}\/||g" > ${TMP_DEST})
+TAR_DIR="accords-${VERSION}"
+TAR_NAME="${TAR_DIR}.tar.gz"
+
+pushd ${BUILD_DIR}
+	tar xf ${ORIG_DIR}/${TAR_NAME} -C . || (echo "Cannot extract tarball" && exit 3)
+	${TAR_DIR}/configure || (echo "Cannot configure target" && exit 4)
+popd
+
+SOURCE_FILES=$(find ${SOURCE_DIR} -type f -prune -not -path "${SOURCE_DIR}/.git/*" -and -prune -not -name ".gitignore" | sed -e "s|${SOURCE_DIR}/||g" | sort > ${TMP_SOURCE})
+DEST_FILES=$(find ${BUILD_DIR}/${TAR_DIR}/ -type f | sed -e "s|${BUILD_DIR}/${TAR_DIR}/||g" | sort > ${TMP_DEST})
 
 echo "The following files might be missing from Makefile's DIST variable"
 diff -uw ${TMP_SOURCE} ${TMP_DEST} | grep "^-"
 
 rm ${TMP_SOURCE} || echo "Error while removing ${TMP_SOURCE}"
 rm ${TMP_DEST} || echo "Error while removing ${TMP_DEST}"
+rm -rf ${ORIG_DIR}
+rm -rf ${BUILD_DIR}
