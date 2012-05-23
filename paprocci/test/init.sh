6b161e734ed8430cb4894aa20b2cd392cb8e78da
Mauricio Jost <mauricio.jost@inria.fr>
2012-05-23 23:09:52 +0000

    Changed accordingly autotool files to create also paprocci binaries.
    Signed-off-by: Mauricio Jost <mauricio.jost@inria.fr>

---
+++ paprocci/test/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+INITDIR=`dirname $0`
+BASEDIR=$INITDIR/../../
+
+echo Remember to run this as 
+echo '$ source init.sh'
+
+echo Root directory is $BASEDIR
+
+echo REMEMBER TO DO 
+echo 'export LD_LIBRARY_PATH=/usr/local/lib'
+
+echo Copying xsd files locally...
+cp xsds/* /tmp/
+
+echo Copying xml files locally...
+cp xmls/* $BASEDIR 
+
+echo Copying config files...
+cp configs/* $BASEDIR/scripts
+
+echo Copying manifests...
+cp manifests/* $BASEDIR/scripts
+
+echo Done.
