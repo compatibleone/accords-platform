1ba70e8a3d145c6ae829601076c89f15f5d8150d
Mauricio Jost <mauricio.jost@inria.fr>
2012-06-11 12:11:14 +0000

    Lock mechanism. Everytime COSACS is executed by ProActive, it creates a file /home/cosacs/lock that allows the Selection Script that runs on the node to identify if in that node there is already a running version of COSACS, and hence avoid it. Signed-off-by: Mauricio Jost <mauricio.jost@inria.fr>

--- paprocci/test/pademo_place_cosacs.sh
+++ paprocci/test/pademo_place_cosacs.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 echo Demo ProActive
 echo This script generates cosacs module and places it in the local host to be called by ACCORDS. 
-mkdir -p /home/cosacs/
-cd ../cosacs/
+echo Building COSACS...
+cd ../../cosacs/
 make
 ./build.cosacs.tarball 
+
+echo Placing COSACS locally...
+mkdir -p /home/cosacs/
 rm -fr /home/cosacs/* 
 mv cosacstarball.tgz /home/cosacs/
 cd /home/cosacs/
 tar -xvf cosacstarball.tgz 
 ./install-cosacs
 chmod 777 /home/cosacs/rest
+
+echo Going back to previous folder...
 cd -
-cd ../scripts/
+cd ../paprocci/test/
+cp to-cosacs/* /home/cosacs
+
+echo Done. 
 
