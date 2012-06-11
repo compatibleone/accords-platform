57b08753606435a969bea98f502b69461a71c63a
Mauricio Jost <mauricio.jost@inria.fr>
2012-06-11 13:43:53 +0000

    Solved an issue with the processes run by COSACS (now they are hooked to the existence of the /home/cosacs/lock file). Signed-off-by: Mauricio Jost <mauricio.jost@inria.fr>

--- paprocci/test/to-cosacs/starthooked.sh
+++ paprocci/test/to-cosacs/starthooked.sh
@@ -2,23 +2,22 @@
 
 # starthooked 'command-line of the process to start with a hook to COSACS'
 # This script starts a target process, and makes sure this process is alive if and only if 
-# the COSACS process is still there. Otherwise the target process is killed. 
+# the COSACS process is still there (by checking the LOCKFILE present). 
+# Otherwise the target process is killed. 
 
 TARGET=$@ # This is the target process, the one that will end once COSACS ends. 
-
-COSACS_PID=`ps -ef | grep cosacs.xml | grep -v grep | tee log_cosacs_found.txt  | awk {'print $2'} | sort -n | head -1`
-#COSACS_PID=`ps -ef | grep cosacs.xml | grep -v grep | tee loscosacs.txt     | awk {'print $2'} `
-echo COSACS_PID is $COSACS_PID
+LOCKFILE=/home/cosacs/lock  
 
 # Check if COSACS_PID exists. 
-if [ "x$COSACS_PID" = "x" ]; then 
-#if [ -z "${COSACS_PID+xxx}" ]; then 
-     echo No COSACS running... Exiting...
-     exit 0;
+if [ -e $LOCKFILE ]; then
+  echo COSACS running... 
+else
+echo No COSACS running... Exiting...
+  exit 0;
 fi
 
 # Launch the target and keep the TARGET_PID.
-$TARGET &> log_target.txt &
+$TARGET &> log_target.txt & 
 TARGET_PID=$!
 
 echo Launched target $TARGET with PID $TARGET_PID
@@ -26,7 +25,7 @@ echo Launched target $TARGET with PID $TARGET_PID
 # Check periodically if COSACS_PID exists, and if it doesn't just 
 # kill the TARGET_PID. 
 echo Waiting for COSACS to be killed to kill the target process... 
-strace -p $COSACS_PID 
+inotifywait -e delete $LOCKFILE
 
 echo COSACS was killed, killing the target process...
 kill -9 $TARGET_PID
