4ab5b94dbc0a99553ef74b1803fd68843e080d03
Mauricio Jost <mauricio.jost@inria.fr>
2012-06-13 16:27:23 +0000

    Added a manifest that uses two nodes. Solved an issue in the script to start the chat server. Signed-off-by: Mauricio Jost <mauricio.jost@inria.fr>

--- paprocci/test/to-cosacs/startchat.sh
+++ paprocci/test/to-cosacs/startchat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CURRDIR=`dirname $0`
-nohup $CURRDIR/starthooked.sh chatplus_server -l vizcacha.cloud.sophia.inria.fr -p 8086 CompatibleOneROOM &> log_chat_hooked.txt &
+nohup $CURRDIR/starthooked.sh chatplus_server -l `hostname` -p 8086 CompatibleOneROOM &> log_chat_hooked.txt &
