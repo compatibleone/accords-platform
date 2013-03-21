788d56a512434a9480da78b4a69a3f930aebe220
Hamid MEDJAHED <hmedjahed@prologue.fr>
2013-03-21 14:43:37 +0100

    pyaccordsSDK update

--- pyaccords/pysrc/listoccibuilder.h
+++ pyaccords/pysrc/listoccibuilder.h
@@ -4,20 +4,20 @@
 #include "listcateg.h"
 #include "cordsocci.c"
 
-public struct occi_category * occi_cords_amazonEc2_builder(char * a,char *b);
-public struct occi_category * occi_cords_ec2config_builder(char * a,char *b);
-public struct occi_category * occi_cords_gw_builder(char * a,char *b);
-public struct occi_category * occi_cords_linkgw_builder(char * a,char *b);
-public struct occi_category * occi_cords_intercloudGW_builder(char * a,char *b);
+public struct occi_category * occi_amazonEc2_builder(char * a,char *b);
+public struct occi_category * occi_ec2config_builder(char * a,char *b);
+public struct occi_category * occi_gw_builder(char * a,char *b);
+public struct occi_category * occi_linkgw_builder(char * a,char *b);
+public struct occi_category * occi_intercloudGW_builder(char * a,char *b);
 const static struct {
 	const char *name;
 	public struct occi_category * (*func)(char *a,char * b);
 } occiCategoryBuilder_map[]={
-	{ "amazonEc2", occi_cords_amazonEc2_builder },
-	{ "ec2config", occi_cords_ec2config_builder },
-	{ "gw", occi_cords_gw_builder },
-	{ "linkgw", occi_cords_linkgw_builder },
-	{ "intercloudGW", occi_cords_intercloudGW_builder },
+	{ "amazonEc2", occi_amazonEc2_builder },
+	{ "ec2config", occi_ec2config_builder },
+	{ "gw", occi_gw_builder },
+	{ "linkgw", occi_linkgw_builder },
+	{ "intercloudGW", occi_intercloudGW_builder },
 };
 
 #endif
