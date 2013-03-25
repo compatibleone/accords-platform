ef09a0dbadd624ec8f8d7342a1cc9a171558f102
Hamid MEDJAHED <hmedjahed@prologue.fr>
2013-03-25 15:45:40 +0100

    pyaccordsSDK update

--- pyaccords/pysrc/ec2configAct.py
+++ pyaccords/pysrc/ec2configAct.py
@@ -26,7 +26,7 @@ def current(categStr):
 	l=categStr.split(",")
 	categoryAtr = Cec2config(l[0],l[1],l[2],l[3],l[4],l[5],l[6],l[7],l[8],l[9],l[10],l[11],l[12],l[13],l[14],l[15] )
 	resCateg = ec2config_current(categoryAtr)
-	restResp = [str(restCateg.status),str(restCateg.message)]	
+	restResp = [str(resCateg.status),str(resCateg.message)]	
 	restResponse = ",".join(restResp)
 	return restResponse
 
