8ab0bbe61d023cd299d9c4a6da90c6210cb2ff36
Iain James Marshall <ijm667@hotmail.com>
2013-05-08 11:31:22 +0200

    update of license information for strukt in comodel and codegen tools

--- comodel/src/strukt.h
+++ comodel/src/strukt.h
@@ -1,18 +1,21 @@
-/* ------------------------------------------------------------------------------------	*/
-/*				 CompatibleOne Cloudware				*/
-/* ------------------------------------------------------------------------------------ */
-/*											*/
-/* Ce fichier fait partie de ce(tte) oeuvre de Iain James Marshall et est mise a 	*/
-/* disposition selon les termes de la licence Creative Commons Paternit‚ : 		*/
-/*											*/
-/*			 	Pas d'Utilisation Commerciale 				*/
-/*				Pas de Modification 					*/
-/*				3.0 non transcrit.					*/
-/*											*/
-/* ------------------------------------------------------------------------------------ */
-/* 			Copyright (c) 2011 Iain James Marshall for Prologue 		*/
-/*				   All rights reserved					*/
-/* ------------------------------------------------------------------------------------ */
+/* -------------------------------------------------------------------- */
+/*  ACCORDS PLATFORM                                                    */
+/*  (C) 2011 by Iain James Marshall (Prologue) <ijm667@hotmail.com>     */
+/* -------------------------------------------------------------------- */
+/* Licensed under the Apache License, Version 2.0 (the "License"); 	*/
+/* you may not use this file except in compliance with the License. 	*/
+/* You may obtain a copy of the License at 				*/
+/*  									*/
+/*  http://www.apache.org/licenses/LICENSE-2.0 				*/
+/*  									*/
+/* Unless required by applicable law or agreed to in writing, software 	*/
+/* distributed under the License is distributed on an "AS IS" BASIS, 	*/
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 	*/
+/* implied. 								*/
+/* See the License for the specific language governing permissions and 	*/
+/* limitations under the License. 					*/
+/* -------------------------------------------------------------------- */
+
 #ifndef	_strukt_h
 #define	_strukt_h
 
--- tools/strukt/struktsql.c
+++ tools/strukt/struktsql.c
@@ -19,6 +19,7 @@
 /* 02110-1301 USA, or see the FSF site: http://www.fsf.org.			*/
 /*										*/
 /* ---------------------------------------------------------------------------- */
+
 #ifndef	_strukt_sql_c
 #define	_strukt_sql_c
 
