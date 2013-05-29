d4abb6ec37f2ef698bf6ccee34a6895164c3a377
Stefan Ross <stefan.ross@gmail.com>
2013-05-29 15:00:50 +0100

    Refactor into class, add method for cleaning database

--- python/parsing/utils.py
+++ python/parsing/utils.py
@@ -7,80 +7,127 @@ publication_port = '8086'
 _publication_loc = "publication"
 _all_loc = '-'
 
-def get_all():
-    return _get(_all_loc)
+def _location(root, category, ident = ''):
+    return root + '/' + category + '/' + ident
 
-def _get(category, id = '', filters = {}, root = _request_root + ':' + publication_port):
-    headers = _headers_with_attributes(filters)
-    url = root + '/' + category + '/' + id
-    return requests.get(url, headers = headers)
+def _get_match(match, count):
+    if match is not None and len(match.groups()) > count:
+        return match.groups()[count]
+    return None
    
 def _find_attribute_in_response(response, regex):        
     match = re.search(regex, response)
-    if len(match.groups()) > 0:
-        return match.groups()[0]
-    return None
+    return _get_match(match, 0)
 
 _find_server_regex = u"X-OCCI-Attribute: occi.publication.why=\"(\S+)\"\n"
-def _find_server_of_entry(id):
-    r = _get(_publication_loc, id)
-    return _find_attribute_in_response(r.text, _find_server_regex)
 
-#TODO Rename
-def _find_attribute2_in_response(response, regex):        
+def _find_location_id_in_response(response, regex):        
     match = re.search(regex, response)
-    if len(match.groups()) > 1:
-        return match.groups()[1]
-    return None
+    return _get_match(match, 1)
 
 _find_id_regex = u"X-OCCI-Location: (\S+)/(\S+)\n"
 def _find_id_of_entry(response):
-    return _find_attribute2_in_response(response, _find_id_regex)
+    return _find_location_id_in_response(response, _find_id_regex)
 
 def _find_all_ids_of_entries(response):        
     matches = re.finditer(_find_id_regex, response)
     return [match.groups()[1] for match in matches]
-
-def find_root_of_category(category):
-    r = _get(_publication_loc, filters = {'occi.publication.what':category})
-    if r.status_code is not requests.codes.ok:
-        return None
-    ids = _find_all_ids_of_entries(r.text)
-    servers = [_find_server_of_entry(id) for id in ids]
-    if len(servers) is 0 or any(server != servers[0] for server in servers):
-        return None
-    return servers[0]
     
 #TODO This doesn't work properly...since all values have the same key, only the last is actually used
 # Could do separate gets for each request, and then manually union them!
-def _headers_with_attributes(attributes):
+def _headers_with_attributes(attributes, authorization = None):
     #comma_separated_attributes = ','.join(('{0}={1}'.format(name, value) for name, value in attributes.items()))
     #headers = {'X-OCCI-Attribute':comma_separated_attributes}
-    headers = dict([['X-OCCI-Attribute', '{0}={1}'.format(name, value)] for name, value in attributes.items()])
+    attributes = [['X-OCCI-ATTRIBUTE', '{0}={1}'.format(name, value)] for name, value in attributes.items()]
+    authorization_header = []
+    if authorization is not None:
+        authorization_header = [['X-OCCI-AUTHORIZE', authorization]] 
+    headers = dict(attributes + authorization_header)
     #headers = dict([['X-OCCI-Attribute: {0}'.format(name), '={0}'.format(value)] for name, value in attributes.items()])
     return headers
+    
+class Provisioner(object):
+    user_name = 'test-parser'
+    
+    def __init__(self):
+        self.auth_string = None
+    
+    def _authorize(self):
+        self.auth_string = self.authorize(self.user_name)        
+    
+    def lookup_all(self, node_name = '"cn_any"'):  
+        self._authorize()      
+        node = self.find_id('node', node_name)
+        if node is None:
+            raise LookupError('Unable to find node "{0}"'.format(node_name))
+        return False
+        
+    def get_all(self):
+        return self._get(_all_loc)
 
-def get_marketplace_users():
-    all = get_all() #TODO Is this required?  C parser does it
+    def _find_server_of_category(self, ident):
+        r = self._get(_publication_loc, ident)
+        return _find_attribute_in_response(r.text, _find_server_regex)
+
+    def _get(self, category, ident = '', filters = {}, root = _request_root + ':' + publication_port):
+        headers = _headers_with_attributes(filters, self.auth_string)
+        url = _location(root, category, ident)
+        return requests.get(url, headers = headers)
+
+    def find_root_of_category(self, category):
+        r = self._get(_publication_loc, filters = {'occi.publication.what':category})
+        if r.status_code is not requests.codes.ok:
+            raise LookupError("Couldn't find server for category '{0}'".format(category))
+        ids = _find_all_ids_of_entries(r.text)
+        servers = [self._find_server_of_category(ident) for ident in ids]
+        if len(servers) is 0 or any(server != servers[0] for server in servers):
+            raise LookupError("Conflicting servers found for category '{0}'".format(category))
+        return servers[0]
     
-    # TODO Skip where since for now it seems that where is always marketplace 
-    #attributes = {'occi.publication.where':'marketplace', 'occi.publication.what':'user'}
-    attributes = {'occi.publication.what':'user'}
-    r = _get(_publication_loc, filters = attributes)
+    def _request_entry(self, category, entry):
+        root = self.find_root_of_category(category)
+        
+        r = self._get(category, filters = {'occi.{0}.name'.format(category):entry}, root = root)
+        ident = None if r.status_code is not requests.codes.ok else _find_id_of_entry(r.text)
+        return ident, root
+            
+    def find_id(self, category, entry):    
+        ident, _ = self._request_entry(category, entry)
+        return ident
+        
+
+    def _location_of_entry(self, category, entry):
+        ident, root = self._request_entry(category, entry)
+        if ident is None:
+            return None
+        location = _location(root, category, ident)
+        return location
+
+    def authorize(self, user):
+        auth_location = self._location_of_entry('user', user)
+        if auth_location is None:
+            raise LookupError("Unable to find entry for user '{0}'".format(entry))
+        authentication_server = self.find_root_of_category('authorization')
+        if authentication_server is None:
+            raise LookupError("Authentication server not found")
+        post_url = _location(authentication_server, 'authorization')
+        post_headers = _headers_with_attributes({'occi.authorization.user':auth_location})
+        r = requests.post(post_url, headers = post_headers)
+        if r.status_code is not requests.codes.ok:
+            return None
+        return post_url + _find_id_of_entry(r.text)
     
-    return _find_all_ids_of_entries(r.text) if r.status_code is requests.codes.ok else None
+    def get_marketplace_users(self):
+        all = self.get_all() #TODO Is this required?  C parser does it
+        
+        # TODO Skip where since for now it seems that where is always marketplace 
+        #attributes = {'occi.publication.where':'marketplace', 'occi.publication.what':'user'}
+        attributes = {'occi.publication.what':'user'}
+        r = self._get(_publication_loc, filters = attributes)
+        
+        return _find_all_ids_of_entries(r.text) if r.status_code is requests.codes.ok else None
     
-def find_id(category, entry):
-    root = find_root_of_category(category)
-    
-    r = _get(category, filters = {'occi.{0}.name'.format(category):entry}, root = root)
-    if r.status_code is not requests.codes.ok:
-        return None
-    return _find_id_of_entry(r.text)
-    
-    
-    
-    
-    
-    
-    
+    def clean(self):
+        location = self._location_of_entry('manifest', 'cn_any')
+        if location is not None:
+            r = requests.delete(location)
--- python/test/TestParsing.py
+++ python/test/TestParsing.py
@@ -8,20 +8,25 @@ import parsing.utils
 
 class TestInterface(unittest.TestCase):
     '''Tests basic communication with the servers'''
+    def setUp(self):
+        self.dut = parsing.utils.Provisioner()
     
     def test_that_get_all_on_publication_server_returns_status_okay(self):
-        r = parsing.utils.get_all()
+        r = self.dut.get_all()
         
         assert_that(r.status_code, is_(requests.codes.ok))
 
-class TestParsing(unittest.TestCase):
+class TestBasicLookupFunctions(unittest.TestCase):
     '''
     Test parsing of manifest files - these tests assume the database is already set up in a sensible state.
     TODO - Remove dependencies on initial database state.
     '''
     
+    def setUp(self):
+        self.dut = parsing.utils.Provisioner()
+        
     def test_that_find_users_returns_list_of_users(self):
-        users = parsing.utils.get_marketplace_users()
+        users = self.dut.get_marketplace_users()
         
         assert_that(len(users), is_(greater_than(0)))
         
@@ -30,19 +35,39 @@ class TestParsing(unittest.TestCase):
         self.fail("Currently don't have a way to filter on multiple attributes")
     
     def test_that_find_root_returns_correct_uri_for_user_category(self):
-        server = parsing.utils.find_root_of_category('user')
+        server = self.dut.find_root_of_category('user')
         
         assert_that(server, is_('http://127.0.0.1:8087'))
         
-    def test_the_find_root_returns_none_for_nonsense_category(self):
-        server = parsing.utils.find_root_of_category('random-rubbish')
-        
-        assert_that(server, is_(None))
+    def test_the_find_root_raises_for_nonsense_category(self):        
+        self.assertRaises(LookupError, self.dut.find_root_of_category, 'random-rubbish')
         
     def test_that_find_id_returns_id_for_user_test_parser(self):
-        id = parsing.utils.find_id('user', 'test-parser')
+        id = self.dut.find_id('user', 'test-parser')
         
         assert_that(id, is_not(None))
+        
+    def test_that_authorize_returns_authorization_location(self):
+        authorization = self.dut.authorize('test-parser')
+        
+        assert_that(authorization, is_not(None))
+        
+class TestManifestConstruction(unittest.TestCase):
+    def setUp(self):
+        self.dut = parsing.utils.Provisioner()
+     
+    #TODO 
+    @unittest.skip 
+    def test_that_lookup_all_resources_returns_ok(self):
+        assert_that(self.dut.lookup_all(), is_(True))
+        
+    def test_that_manifest_cn_any_does_not_exist_after_clean(self):
+        
+        self.dut.clean()        
+        id = self.dut.find_id('manifest', 'cn_any')
+        
+        assert_that(id, is_(None))
+        
         
         
     
