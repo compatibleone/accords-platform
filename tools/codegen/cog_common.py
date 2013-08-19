a7678dc11202e2e30ce6e1d016141d1b8335c58c
Stefan Ross <stefan.ross@gmail.com>
2013-08-19 09:41:30 +0100

    CONNECT-289 Generate node_backend.h files

--- tools/codegen/cog_common.py
+++ tools/codegen/cog_common.py
@@ -10,6 +10,7 @@ import codegen_types.ctypes as ctypes
 # Example invocations from tools directiory:
 # python -m cogapp -ed -D cog_category_file=publication.h -D model_dir="../model" -o tmp/output.xsd cog/templates/schema.xsd
 # python -m cogapp -ed -D cog_category_file=publication.h -D model_dir='../model' -o tmp/output.h cog/templates/category.h 
+# python -m cogapp -ed -D cog_category_file=publication.h -D model_dir='../model' -o tmp/publication_node_backend.h cog/templates/node_backend.h 
 
 
 def init_models(model_dir):
@@ -21,16 +22,30 @@ def _type_conversion(type_name):
         return "integer"
     return type_name
 
+def _category_name(category_file):
+    return category_for_file(category_file, models).struct_name     
+
 def category_xsd(category_file):
     category = category_for_file(category_file, models)   
     cog.outl('<xsd:complexType name="{0}">'.format(category.struct_name))
     for name, type_name in category.backend_type_list():
         cog.outl(r'    <xsd:attribute name="{0}" type="xsd:{1}"/>'.format(name, _type_conversion(type_name)))
 
+
+def node_h_guard(category_file):
+    category = category_for_file(category_file, models)
+    
+    _generic_h_guard('{0}_node_backend'.format(category.struct_name))
+    
 def category_h_guard(category_file):
     category = category_for_file(category_file, models)
     
-    guard = category.struct_name.lower() + "_h" # TODO Could just use the file name
+    _generic_h_guard(category.struct_name)
+    
+def _generic_h_guard(filename):
+    filename_root, _ = os.path.splitext(filename)
+    guard = '{0}_h'.format(filename_root.lower())
+    
     cog.outl('#ifndef {0}'.format(guard))    
     cog.outl('#define {0}'.format(guard))
     
@@ -44,3 +59,7 @@ def category_h_members(category_file):
     
     for name, type_name in category.backend_type_list():
         cog.outl('{0} {1};'.format(ctypes.from_platform_type(type_name), name))
+
+def node_interface_declaration(category_file):
+    name = _category_name(category_file)
+    cog.outl("struct {0}_backend_interface *{1}_node_interface_func();".format(name, name))
--- tools/cog/templates/node_backend.h
+++ tools/cog/templates/node_backend.h
@@ -1,8 +1,7 @@
+[[[cog import codegen.cog_common as t; t.init_models(model_dir); t.node_h_guard(cog_category_file) ]]]
+[[[end]]]
 
-#ifndef _node_backend_h
-#define _node_backend_h
-
-struct publication_backend_interface *node_interface_func();
-
+[[[cog t.node_interface_declaration(cog_category_file)]]]
+[[[end]]]
 
 #endif
