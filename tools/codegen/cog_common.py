b6d0576a91c5d97b2902481e19795b4aaefd54cd
Stefan Ross <stefan.ross@gmail.com>
2013-09-24 13:41:55 +0100

    CONNECT-157 Use model parsing to decide which backends to use

--- tools/codegen/cog_common.py
+++ tools/codegen/cog_common.py
@@ -431,8 +431,7 @@ def link_backend():
     return _category_name() == "cords_xlink"
 
 def riak_backend():
-    #return (_category_name() == 'cords_publication') or link_backend() #TODO Hardcoding switch for now
-    return True
+    return category.backend.plugin == 'riak'
          
 def backend_include():    
     if not riak_backend():
