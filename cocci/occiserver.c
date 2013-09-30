3c076afe41a435ca975cb88c10d459e30e793a0d
Iain James Marshall <ijm667@hotmail.com>
2013-09-30 15:58:46 +0200

    final correction for double delete

--- cocci/occiserver.c
+++ cocci/occiserver.c
@@ -1402,7 +1402,7 @@ private	int	occi_untrack_double_delete( struct occi_element * eptr )
 	else	eptr->previous->next = eptr->next;
 	if ( eptr->next )
 		eptr->next->previous = eptr->previous;
-	occi_liberate_element( eptr );
+	occi_remove_element( eptr );
 	pthread_mutex_unlock( &DeleteLock );
 	return( 0 );
 }
