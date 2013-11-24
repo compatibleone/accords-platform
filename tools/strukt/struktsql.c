1209fde8da9c71b0920702d29625c2e78112bfb0
Iain James Marshall <ijm667@hotmail.com>
2013-11-24 20:47:59 +0100

    correction in backend list handling

--- tools/strukt/struktsql.c
+++ tools/strukt/struktsql.c
@@ -834,9 +834,9 @@ public	void	generate_occi_sql_builder( FILE * h, char * nptr )
 		fprintf(h,"public struct  occi_%s_node * occi_first_%s_node() { return(&KindNode); }\n",
 			C.klass,C.klass);
 		fprintf(h,"public struct  occi_%s_node * occi_next_%s_node(struct occi_%s_node * nptr ) { return( &KindNode ); }\n",
-			C.klass,C.klass);
+			C.klass,C.klass,C.klass);
 		fprintf(h,"public struct  occi_%s_node * occi_previous_%s_node(struct occi_%s_node * nptr ) { return( &KindNode ); }\n",
-			C.klass,C.klass);
+			C.klass,C.klass,C.klass);
 		fprintf(h,"public struct  occi_%s_node * occi_last_%s_node() { return( &KindNode ); }\n",
 			C.klass,C.klass);
 		break;
@@ -844,9 +844,9 @@ public	void	generate_occi_sql_builder( FILE * h, char * nptr )
 		fprintf(h,"public struct  occi_%s_node * occi_first_%s_node() { return(&KindNode); }\n",
 			C.klass,C.name);
 		fprintf(h,"public struct  occi_%s_node * occi_next_%s_node(struct occi_%s_node * nptr ) { return( &KindNode ); }\n",
-			C.klass,C.name);
+			C.klass,C.name,C.klass);
 		fprintf(h,"public struct  occi_%s_node * occi_previous_%s_node(struct occi_%s_node * nptr ) { return( &KindNode ); }\n",
-			C.klass,C.name);
+			C.klass,C.name,C.klass);
 		fprintf(h,"public struct  occi_%s_node * occi_last_%s_node() { return( &KindNode ); }\n",
 			C.klass,C.name);
 		break;
