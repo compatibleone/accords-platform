cee96ec7756e747ac6fedf9347b56937eb376c9e
Vincent RENARDIAS <vrenardias@citypassenger.com>
2014-01-27 12:10:01 +0100

    Script to purge all CompatibleOne packages and config.

---
+++ debian/purge.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+dpkg -P $(dpkg -l \*compatibleone\*|grep compatibleone|awk '{print $2}') libcocspi0 libcocspi-dev libcocarrier0 libcocarrier-dev libcocci0 libcocci-dev libcopabr0 libcopabr-dev libcords0 libcords-dev libcorest0 libcorest-dev libcoxml0 libcoxml-dev libocci0 libocci-dev libpubocci0 libpubocci-dev cosacs
+rm -rf /var/l{ib,og}/accords /etc/accords
+exit 0
